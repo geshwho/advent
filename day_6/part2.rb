da62c506d4c107061456eb06b2eab9fb73dc7956
Mangesh Tamhankar <mangesh@gusto.com>
2020-12-06 10:12:54 -0500

    Day 6

---
+++ day_6/part2.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+filename = ARGV.first
+file = File.open(filename)
+
+answers = file.readlines.map(&:strip)
+
+blanks = answers.each_index.select { |index| answers[index] == '' }
+
+n = 0
+count = 0
+
+while n <= blanks.size
+  start = n.zero? ? 0 : blanks[n - 1] + 1
+  finish = n == blanks.size ? answers.size - 1 : blanks[n] - 1
+  answer = answers[start..finish]
+  n += 1
+
+  count += answer.inject(('a'..'z').to_a) { |i, n| i & n.chars }.size
+end
+
+puts count
